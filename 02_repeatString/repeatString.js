5b1433d5687dc8d7c3b75d767289026d0f4975d1
duckybanana <139942767+duckybanana@users.noreply.github.com>
2023-08-27 22:30:06 +0800

    Update 1st test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+  let result = '';
+  for (let i=0; i < num; i++) {
+    result += string
+  }
+  console.log(result)
 };
 
 // Do not edit below this line
