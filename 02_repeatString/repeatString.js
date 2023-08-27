53191cca213ec20cd8c6b0232d0e224b65e0d5bc
duckybanana <139942767+duckybanana@users.noreply.github.com>
2023-08-27 22:35:30 +0800

    Add single quotes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -5,7 +5,7 @@ const repeatString = function(string, num) {
   for (let i=0; i < num; i++) {
     result += string
   }
-  console.log(result)
+  console.log(`'${result}'`)
 };
 
 // Do not edit below this line
